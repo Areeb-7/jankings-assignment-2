70ec4b4e0a75a97a28b6233d1d3689a4338115e4
Areeb Shah <2023.shahm@isu.ac.in>
2026-06-13 17:16:41 +0530

    Initial commit

---
+++ serviceProviders/serviceProviderbackend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import connectDB from './dbconnect.js';
+import router from './router/router.js';
+import cors from 'cors'
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+app.use(cors())
+
+// Middleware
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+// Routes
+app.use('/api/service-providers', router);
+
+// Start server
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
